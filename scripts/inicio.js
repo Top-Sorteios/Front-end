465594b79f3dbf95dea3b6e8b7f19ebb86c1d638
flkvio <105506000+flkvio@users.noreply.github.com>
2024-06-06 23:34:59 -0300

    email e token passam a ser armazenados. Botão redirecionando pra Gestão de

---
+++ scripts/inicio.js
@@ -0,0 +1,2 @@
+console.log(sessionStorage.getItem("email"));
+console.log(sessionStorage.getItem("token"));
--- scripts/login.js
+++ scripts/login.js
@@ -8,17 +8,28 @@ const inputSenha = document.querySelector("#senha");
 const entrar = async function () {
   let url = SERVER_NAME + "usuarios/login";
 
-  const request = await fetch(url, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      email: inputEmail.value,
-      senha: inputSenha.value,
-    }),
-  });
+  try {
+    const request = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: inputEmail.value,
+        senha: inputSenha.value,
+      }),
+    });
 
-  const response = await request.json();
-  console.log(response);
+    const response = await request.json();
+
+    if (response.status) {
+      sessionStorage.setItem("email", response.email);
+      sessionStorage.setItem("token", response.token);
+      window.location.assign("../home/inicio.html")
+    }
+
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 //serializar = transformar em texto
