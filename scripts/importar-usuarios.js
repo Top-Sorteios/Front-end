8ac68e62b31cdc8373efb880cde1b628bc6dd799
Flávio <ffs.elpidio@gmail.com>
2024-06-14 17:24:57 -0300

    Alterações na importação de usuários

--- scripts/importar-usuarios.js
+++ scripts/importar-usuarios.js
@@ -1,16 +1,25 @@
 import SERVER_NAME from "./CONSTANTES";
 
+const formImportarUsuarios = document.querySelector("#form-importar-usuarios");
+
 const importarUsuarios = async function () {
-  const arquivo = document.querySelector("#importar-csv").files[0];
   let url = `${SERVER_NAME}usuarios/importar-usuario/${sessionStorage.getItem(
     "email"
   )}`;
+  const arquivo = document.querySelector("#importar-csv").files[0];
+  const formData = new FormData();
+  formData.append("file", arquivo);
 
   const request = await fetch(url, {
     method: "POST",
-    headers: {
-      "Content-Type": "form-data",
-      body: new FormData().append("file", arquivo),
-    },
+    body: formData,
   });
+
+  const response = await request.json();
+  console.log(response);
 };
+
+formImportarUsuarios.addEventListener("submit", (event) => {
+  event.preventDefault();
+  importarUsuarios();
+});
