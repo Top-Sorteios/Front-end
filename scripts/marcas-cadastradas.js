a1f849e8155aa7d06ea709615ecc2c2f75fbf884
Felipe-Bonatto <felipe.bonatto@gmail.com>
2024-06-22 02:03:43 -0300

    Marcas esta faltando somente o upload das imagens

--- scripts/marcas-cadastradas.js
+++ scripts/marcas-cadastradas.js
@@ -1,4 +1,4 @@
-import {SERVER_NAME, TOKEN} from "./CONSTANTES.js";
+import { SERVER_NAME, TOKEN } from "./CONSTANTES.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
     // const token = sessionStorage.getItem("token");
@@ -31,6 +31,8 @@ const createTable = async (item) => {
         let txtCriadoEm = document.createTextNode(marcas.criadoEm)
         let txtEditar = document.createTextNode('Editar')
 
+        tr.setAttribute('class', 'filtrarTr')
+        tdNome.setAttribute('class', 'filtrarNome')
         linkEditar.setAttribute('href', '#')
         linkEditar.setAttribute('idmarca', marcas.id)
 
@@ -51,3 +53,23 @@ const createTable = async (item) => {
         })
     });
 }
+
+const pesquisar = () => {
+    let inputPesquisa = document.getElementById('input-pesquisa').value.toLowerCase()
+    const listMarca = document.querySelectorAll('.filtrarTr')
+    let listNome = document.querySelectorAll('.filtrarNome')
+
+    for (let i = 0; i < listMarca.length; i++) {
+        if (!listNome[i].innerHTML.toLowerCase().includes(inputPesquisa)) {
+            listMarca[i].classList.add('none')
+        } else {
+            listMarca[i].classList.remove('none')
+        }
+    }
+}
+
+const inputPesquisa = document.getElementById('input-pesquisa')
+inputPesquisa.addEventListener('keyup', () => {
+    pesquisar()
+})
+
