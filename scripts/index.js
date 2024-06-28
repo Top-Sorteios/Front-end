7527ad077b818fe7c472ba769bdcc41405eefcaf
Flávio <ffs.elpidio@gmail.com>
2024-06-28 19:03:58 -0300

    Alterações no visual

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,14 @@
 import { SERVER_NAME, get } from "./CONSTANTES.js";
 
 const marcasParceirasContainer = document.querySelector("#logo-marcas-parceiras");
+const botaoEntrar = document.querySelector("#login");
+
+botaoEntrar.addEventListener("click", (event)=>{
+    console.log("Foi clickado");
+    event.preventDefault()
+    window.location.assign("./html/login/index.html")
+})
+
 
 const getMarcasParceiras = async function(){
     const request = await get("marcas/obter", true);
--- scripts/trocar-senha.js
+++ scripts/trocar-senha.js
@@ -1,39 +1,49 @@
-import {SERVER_NAME} from "./CONSTANTES.js";
+import { SERVER_NAME } from "./CONSTANTES.js";
 
-document.addEventListener('DOMContentLoaded', async () => {
-    const token = sessionStorage.getItem("token");
-    const email = sessionStorage.getItem("email");
+document.addEventListener("DOMContentLoaded", async () => {
+  const token = sessionStorage.getItem("token");
+  const email = sessionStorage.getItem("email");
 
-    if (!token || !email) {
-        alert('Usuário não autenticado');
-        window.location.assign('../login/index.html');
-        return;
+  if (!token || !email) {
+    alert("Usuário não autenticado");
+    window.location.assign("../login/index.html");
+    return;
+  }
+
+  try {
+    const url = `${SERVER_NAME}usuarios/obter/${email}`;
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Falha ao obter informações do usuário");
     }
 
-    try {
-        const url = `${SERVER_NAME}usuarios/obter/${email}`;
-        const response = await fetch(url, {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json"
-            }
-        });
+    const userData = await response.json();
+    document.getElementById("nome").value = userData.nome;
+    document.getElementById("data-nascimento").value = userData.datanascimento;
+    document.getElementById("cpf").value = userData.cpf;
+    document.getElementById("email").value = userData.email;
+    document.getElementById("turma").value = userData.turma.nome;
 
-        if (!response.ok) {
-            throw new Error('Falha ao obter informações do usuário');
-        }
-
-        const userData = await response.json();
-        document.getElementById('nome').value = userData.nome;
-        document.getElementById('data-nascimento').value = userData.datanascimento;
-        document.getElementById('cpf').value = userData.cpf;
-        document.getElementById('email').value = userData.email;
-        document.getElementById('turma').value = userData.turma.nome;        
-
-    } catch (error) {
-        console.error('Erro ao obter informações do usuário:', error);
-    }
+    // Adicionar lógica para o botão "Cancelar"
+    const cancelarButton = document.getElementById("cancel");
+    cancelarButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      if (userData.administrador) {
+        window.location.assign("../home/inicio-admin.html");
+      } else {
+        window.location.assign("../home/inicio.html");
+      }
+    });
+  } catch (error) {
+    console.error("Erro ao obter informações do usuário:", error);
+  }
 });
 
 const formulario = document.querySelector("#form-troca-senha");
@@ -41,47 +51,47 @@ const inputSenhaAtual = document.querySelector("#senha-atual");
 const inputNovaSenha = document.querySelector("#nova-senha");
 
 async function trocarSenha() {
-    const token = sessionStorage.getItem("token");
-    const email = sessionStorage.getItem("email");
-    const senhaAtual = inputSenhaAtual.value;
-    const novaSenha = inputNovaSenha.value;
+  const token = sessionStorage.getItem("token");
+  const email = sessionStorage.getItem("email");
+  const senhaAtual = inputSenhaAtual.value;
+  const novaSenha = inputNovaSenha.value;
 
-    const url = `${SERVER_NAME}usuarios/editar/senha/${email}`;
-    const senhaData = {
-        senhaAtual: senhaAtual,
-        senha: novaSenha
-    };
+  const url = `${SERVER_NAME}usuarios/editar/senha/${email}`;
+  const senhaData = {
+    senhaAtual: senhaAtual,
+    senha: novaSenha,
+  };
 
-    try {
-        const response = await fetch(url, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify(senhaData)
-        });
+  try {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(senhaData),
+    });
 
-        if (!response.ok) {
-            throw new Error('Falha ao alterar a senha');
-        }
-
-        const contentType = response.headers.get('content-type');
-        if (contentType && contentType.includes('application/json')) {
-            const data = await response.json();
-            console.log('Senha alterada com sucesso:', data);
-            alert('Senha alterada com sucesso!');
-        } else {
-            console.log('Senha alterada com sucesso');
-            alert('Senha alterada com sucesso!');
-        }
-    } catch (error) {
-        console.error('Erro ao alterar a senha:', error);
-        alert('Erro ao alterar a senha: ' + error.message);
+    if (!response.ok) {
+      throw new Error("Falha ao alterar a senha");
     }
+
+    const contentType = response.headers.get("content-type");
+    if (contentType && contentType.includes("application/json")) {
+      const data = await response.json();
+      console.log("Senha alterada com sucesso:", data);
+      alert("Senha alterada com sucesso!");
+    } else {
+      console.log("Senha alterada com sucesso");
+      alert("Senha alterada com sucesso!");
+    }
+  } catch (error) {
+    console.error("Erro ao alterar a senha:", error);
+    alert("Erro ao alterar a senha: " + error.message);
+  }
 }
 
-formulario.addEventListener('submit', function (event) {
-    event.preventDefault();
-    trocarSenha();
+formulario.addEventListener("submit", function (event) {
+  event.preventDefault();
+  trocarSenha();
 });
