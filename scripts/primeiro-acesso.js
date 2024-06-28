ab2bd9e98b2e0acd98f775b33f621537ca5d7478
Flávio <ffs.elpidio@gmail.com>
2024-06-28 20:26:21 -0300

    Alterações no Estilo

--- scripts/primeiro-acesso.js
+++ scripts/primeiro-acesso.js
@@ -48,4 +48,10 @@ formPrimeiroAcesso.addEventListener("submit", (event) => {
 
 });
 
-
+document.getElementById('cpf').addEventListener('input', function (e) {
+  let value = e.target.value.replace(/\D/g, '');
+  value = value.replace(/(\d{3})(\d)/, '$1.$2');
+  value = value.replace(/(\d{3})(\d)/, '$1.$2');
+  value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+  e.target.value = value;
+});
