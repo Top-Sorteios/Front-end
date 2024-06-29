60f2d9527aac98119d21c9a2ee17a503035b5514
Felipe-Bonatto <felipe.bonatto@gmail.com>
2024-06-29 01:51:47 -0300

    atualização padronizando aviso de erros

--- scripts/primeiro-acesso.js
+++ scripts/primeiro-acesso.js
@@ -1,4 +1,4 @@
-import {SERVER_NAME} from "./CONSTANTES.js";
+import { SERVER_NAME } from "./CONSTANTES.js";
 
 const formPrimeiroAcesso = document.querySelector("#primeiro-acesso-form");
 
@@ -7,44 +7,64 @@ const inputCPF = document.querySelector("#cpf");
 const inputDataNascimento = document.querySelector("#data-nascimento");
 const inputSenha = document.querySelector("#senha");
 const inputConfirmarSenha = document.querySelector("#confirmar-senha");
-
+const textoError = document.querySelectorAll('.wrong-text')
+console.log(inputSenha)
 const cadastrar = async function () {
+  limparFormulario()
+  if (inputEmail.value == '') {
+    inputEmail.classList.add('wrong')
+    textoError[0].innerText = 'Digite o email'
+    inputEmail.focus()
+  } else if (inputCPF.value == '') {
+    inputCPF.classList.add('wrong')
+    textoError[1].innerText = 'Digite o CPF'
+    inputCPF.focus()
+  } else {
+
+    let url = SERVER_NAME + "usuarios/primeiro-acesso";
 
 
-  let url = SERVER_NAME + "usuarios/primeiro-acesso";
 
+    const request = await fetch(url, {
+      method: "PUT",
 
-  
-  const request = await fetch(url, {
-    method: "PUT",
-    
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      email: inputEmail.value,
-      cpf: inputCPF.value,
-      datanascimento: inputDataNascimento.value.replaceAll("-","/"),
-      senha: inputSenha.value,
-    }),
-  });
-  const response = await request.json();
-  console.log(response);
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: inputEmail.value,
+        cpf: inputCPF.value,
+        datanascimento: inputDataNascimento.value.replaceAll("-", "/"),
+        senha: inputSenha.value,
+      }),
+    });
+    const response = await request.json();
+    console.log(response);
+  }
 };
 
-const limparFormulario= function(){ 
-    const listaInputs = document.querySelectorAll("input");
-    listaInputs.forEach(input => {
-        input.value = "";
-    })
+const limparFormulario = function() {
+  inputEmail.classList.remove('wrong')
+  inputCPF.classList.remove('wrong')
+  inputSenha.classList.remove('wrong')
+  inputConfirmarSenha.classList.remove('wrong')
+  textoError[0].innerText = ''
+  textoError[1].innerText = ''
+  textoError[2].innerText = ''
+  textoError[3].innerText = ''
 }
+// const limparFormulario = function () {
+//   const listaInputs = document.querySelectorAll("input");
+//   listaInputs.forEach(input => {
+//     input.value = "";
+//   })
+// }
 
 formPrimeiroAcesso.addEventListener("submit", (event) => {
   event.preventDefault();
-    if(inputSenha.value == inputConfirmarSenha.value){
-        cadastrar().then(()=>{
-          limparFormulario()
-        });
-       
-    }
+  if (inputSenha.value == inputConfirmarSenha.value) {
+    cadastrar().then(() => {
+    });
+
+  }
 
 });
 
