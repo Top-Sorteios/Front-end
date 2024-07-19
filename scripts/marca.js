356e5de4b77e8bb3ed566e72595d9b716b1ae0b0
Eduardo Dias Lima <134237144+eduardodiaslima@users.noreply.github.com>
2024-07-19 14:00:19 -0300

    Update marca.js

--- scripts/marca.js
+++ scripts/marca.js
@@ -7,6 +7,7 @@ import {
   mostrarAlert,
 } from "./CONSTANTES.js";
 
+const MAX_FILE_SIZE = 1 * 1024 * 1024;
 
 const error = document.querySelectorAll(".wrong-text");
 const inputNome = document.getElementById("nome-marca");
@@ -20,8 +21,44 @@ const previewLogo = document.getElementById("preview-logo");
 
 const divImgs = document.querySelectorAll(".div-imgs");
 const buttonDelete = document.getElementById("button-remover");
-
 const buttonSave = document.getElementById("button-salvar");
+
+// Função para Converter Base64 para Blob
+const base64ToBlob = (base64String, contentType) => {
+  const byteCharacters = atob(base64String);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  return new Blob([byteArray], { type: contentType });
+};
+
+const handleFileInput = (input, preview, errorIndex) => {
+  const file = input.files[0];
+  if (file.size > MAX_FILE_SIZE) {
+    error[errorIndex].innerText = "O tamanho da imagem não pode exceder 1 MB";
+    input.classList.add("wrong");
+    preview.src = "https://github.com/Top-Sorteios/Front-end/blob/main/assets/images/placeholder-files/placeholder.png?raw=true";
+    buttonSave.disabled = true;
+    return false;
+  } else {
+    error[errorIndex].innerText = "";
+    input.classList.remove("wrong");
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const base64String = reader.result;
+      preview.setAttribute("src", `${base64String}`);
+      reader.abort();
+    };
+    reader.readAsDataURL(file);
+    buttonSave.disabled = false;
+    return true;
+  }
+};
+
+inputBanner.addEventListener("change", () => handleFileInput(inputBanner, previewBanner, 3));
+inputLogo.addEventListener("change", () => handleFileInput(inputLogo, previewLogo, 4));
 
 // Recebe os dados da marca
 const getDados = async () => {
@@ -43,59 +80,19 @@ const getDados = async () => {
     : "https://github.com/Top-Sorteios/Front-end/blob/main/assets/images/placeholder-files/placeholder.png?raw=true";
 };
 
-// Função para Converter Base64 para Blob
-const base64ToBlob = (base64String, contentType) => {
-  const byteCharacters = atob(base64String);
-  const byteNumbers = new Array(byteCharacters.length);
-  for (let i = 0; i < byteCharacters.length; i++) {
-    byteNumbers[i] = byteCharacters.charCodeAt(i);
-  }
-  const byteArray = new Uint8Array(byteNumbers);
-  return new Blob([byteArray], { type: contentType });
-};
-
-inputBanner.addEventListener("change", () => {
-  const reader = new FileReader();
-  const file = inputBanner.files[0];
-  reader.onloadend = () => {
-    const base64String = reader.result;
-    previewBanner.setAttribute("src", `${base64String}`);
-    reader.abort();
-  };
-  reader.readAsDataURL(file);
-});
-
-inputLogo.addEventListener("change", () => {
-  const reader = new FileReader();
-  const file = inputLogo.files[0];
-  reader.onloadend = () => {
-    const base64String = reader.result;
-    previewLogo.setAttribute("src", `${base64String}`);
-    reader.abort();
-  };
-  reader.readAsDataURL(file);
-});
-
 switch (ACAO) {
   case "EDITAR":
     const txtCadastrar = document.querySelector(".txt-cadastrar");
     txtCadastrar.classList.add("none");
     getDados();
-    buttonSave.addEventListener("click", () => {
-        editarMarca()
-
-    });
+    buttonSave.addEventListener("click", () => editarMarca());
     break;
   case "CRIAR":
     const txtEditar = document.querySelector(".txt-editar");
     buttonDelete.style.display = "none";
     txtEditar.classList.add("none");
-    buttonSave.addEventListener("click", () => {
-        cadastrarMarca()
-
-    });
+    buttonSave.addEventListener("click", () => cadastrarMarca());
     break;
-
   default:
     window.location.replace("./marcas-cadastradas.html");
     break;
@@ -119,13 +116,13 @@ const cadastrarMarca = async () => {
     inputOrdemExibicao.classList.add("wrong");
     inputOrdemExibicao.focus();
   } else if (inputBanner.files[0] == null) {
-    divImgs[0].classList.add("wrong");
     error[3].innerText = "Adicione uma imagem";
+    inputBanner.classList.add("wrong");
   } else if (inputLogo.files[0] == null) {
-    divImgs[1].classList.add("wrong");
     error[4].innerText = "Adicione uma imagem";
+    inputLogo.classList.add("wrong");
   } else {
-    buttonSave.disabled = true
+    buttonSave.disabled = true;
     const formData = new FormData();
     formData.append("nome", inputNome.value);
     formData.append("titulo", inputTitulo.value);
@@ -147,7 +144,7 @@ const cadastrarMarca = async () => {
         window.location.assign("../gestao-de-marcas/marcas-cadastradas.html");
       }, 2500);
     } else {
-      buttonSave.disabled = false
+      buttonSave.disabled = false;
       mostrarAlert(
         "Não foi possível cadastrar a marca",
         "fa-solid fa-circle-xmark"
@@ -157,7 +154,6 @@ const cadastrarMarca = async () => {
 };
 
 const editarMarca = async () => {
-
   clearError();
   if (inputNome.value == "") {
     error[0].innerText = "Preencha o campo nome";
@@ -172,7 +168,7 @@ const editarMarca = async () => {
     inputOrdemExibicao.classList.add("wrong");
     inputOrdemExibicao.focus();
   } else {
-    buttonSave.disabled = true
+    buttonSave.disabled = true;
     const formData = new FormData();
     formData.append("nome", inputNome.value);
     formData.append("titulo", inputTitulo.value);
@@ -208,60 +204,39 @@ const editarMarca = async () => {
         window.location.assign("../gestao-de-marcas/marcas-cadastradas.html");
       }, 2500);
     } else {
-      buttonSave.disabled = false
+      buttonSave.disabled = false;
       mostrarAlert(
-        "Não foi possível alterar o registro",
+        "Não foi possível editar a marca",
         "fa-solid fa-circle-xmark"
       );
     }
-    document.body.style.cursor = "auto";
   }
 };
 
-const deleteMarca = async () => {
-  buttonSim.disabled = true
-  const request = await remove(`marcas/${idMarca}`);
-  if (request.status == 200) {
-    mostrarAlert("Marca excluída com sucesso", "fas fa-circle-check");
+// Função para limpar erros
+const clearError = () => {
+  error.forEach((el) => (el.innerText = ""));
+  inputNome.classList.remove("wrong");
+  inputTitulo.classList.remove("wrong");
+  inputOrdemExibicao.classList.remove("wrong");
+  inputBanner.classList.remove("wrong");
+  inputLogo.classList.remove("wrong");
+};
+
+// Remove marca cadastrada
+buttonDelete.addEventListener("click", async () => {
+  const idMarca = sessionStorage.getItem("idMarca");
+  const url = `${SERVER_NAME}marcas/deletar/${idMarca}`;
+  const response = await remove(url);
+  if (response.status == 200) {
+    mostrarAlert("Marca removida com sucesso", "fas fa-circle-check");
     setTimeout(() => {
       window.location.assign("../gestao-de-marcas/marcas-cadastradas.html");
     }, 2500);
   } else {
-    buttonSim.disabled = false
-    mostrarAlert("Não foi possível excluir a marca", "fas fa-circle-xmark");
+    mostrarAlert(
+      "Não foi possível remover a marca",
+      "fa-solid fa-circle-xmark"
+    );
   }
-};
-
-const clearError = () => {
-  error[0].innerText = "";
-  error[1].innerText = "";
-  error[2].innerText = "";
-  error[3].innerText = "";
-  error[4].innerText = "";
-  inputNome.classList.remove("wrong");
-  inputTitulo.classList.remove("wrong");
-  inputOrdemExibicao.classList.remove("wrong");
-  divImgs[0].classList.remove("wrong");
-  divImgs[1].classList.remove("wrong");
-};
-
-buttonDelete.addEventListener("click", () => {
-  let container = document.querySelector(".container");
-  let containerExcluir = document.querySelector(".container-excluir");
-  container.classList.add("none");
-  containerExcluir.classList.remove("none");
-  window.location.assign("#header-marca");
-});
-
-const buttonSim = document.getElementById("button-sim");
-buttonSim.addEventListener("click", () => {
-    deleteMarca();
-});
-
-const buttonNao = document.getElementById("button-nao");
-buttonNao.addEventListener("click", () => {
-  let container = document.querySelector(".container");
-  let containerExcluir = document.querySelector(".container-excluir");
-  container.classList.remove("none");
-  containerExcluir.classList.add("none");
 });
